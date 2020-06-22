a8af7f8d25ae6fa45baa69c94a151e9c35c2585d
navdeep-github <66899480+navdeep-github@users.noreply.github.com>
2020-06-22 18:21:05 +0530

    Add files via upload

---
+++ Firstfile_for_CapstoneProject.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> This file is used for Capstone project in Coursera course</h1>
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print(" Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
